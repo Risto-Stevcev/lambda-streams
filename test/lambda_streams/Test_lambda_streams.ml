7f48c52ac7ac39597cf3bd530b1b2fc2a1a3b343
Risto Stevcev <me@risto.codes>
2020-03-15 22:48:44 +0100

    Split out test files for each package

--- test/lambda_streams/Test_lambda_streams.ml
+++ test/lambda_streams/Test_lambda_streams.ml
@@ -5,7 +5,5 @@ let () =
     [
       "Signal", Test_Signal.suite;
       "Finite.Sync", Test_Finite_Sync.suite;
-      "Finite.Async", Test_Finite_Async.suite;
       "Sync", Test_Sync.suite;
-      "Async", Test_Async.suite;
     ]
